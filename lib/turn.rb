8b2078f2d83df5e6da067fe581c04e8008f263db
Karim Alomari <karim.alomari@gmail.com>
2020-07-13 16:14:22 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,6 +10,7 @@ def input_to_index(user_input)
   user_input.to_i-1
 end
 
+
 def position_taken? (board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     return false
