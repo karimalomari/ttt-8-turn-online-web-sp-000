6b1395e6591b1032f1be66214336b1d602422c69
Karim Alomari <karim.alomari@gmail.com>
2020-07-13 15:59:15 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -16,19 +16,3 @@ def position_taken? (board, index)
   elsif board[index] == "X" || board[index] == "O"
       taken = true
 end
-
-def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
-end
-
-def turn(board)
-  puts "Please enter 1-9:"
-    userInput = gets.strip
-    index = input_to_index(userInput)
-    if !valid_move?(board, index)
-      turn(board)
-    else
-      move(board, index, current_player(board))
-    end
-  display_board(board)
-end
