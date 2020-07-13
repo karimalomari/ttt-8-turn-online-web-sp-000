c7aa340b26db70ed45e0980664d641bc6354bdd5
Karim Alomari <karim.alomari@gmail.com>
2020-07-13 16:08:24 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,15 +11,5 @@ def input_to_index(user_input)
 end
 
 def valid_move?(board, index)
-  if board[index] == "X" || board[index] == "O"&& index.between?(0, 8)
-    return false 
-
-  elsif 
-    board[index] == " "||board[index] == ""|| board[index] == nil && index.between?(0, 8)
-    return true 
-
-  else 
-    return false
-
- end
+  index.between?(0,8) && !position_taken?(board, index)
 end
