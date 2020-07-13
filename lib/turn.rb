8919662ea22c30195172d019f57ffe66fa4ef31b
Karim Alomari <karim.alomari@gmail.com>
2020-07-13 15:57:58 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,7 +12,7 @@ end
 
 def position_taken? (board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
-    taken = false
+      taken = false
   elsif board[index] == "X" || board[index] == "O"
       taken = true
 end
