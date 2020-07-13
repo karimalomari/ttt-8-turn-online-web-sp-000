e0ad26fd2e3a5609393b3c0552a25eed21f90d04
Karim Alomari <karim.alomari@gmail.com>
2020-07-13 16:13:10 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -18,3 +18,16 @@ def position_taken? (board, index)
   end
 end
 
+def valid_move?(board, index)
+  if board[index] == "X" || board[index] == "O"&& index.between?(0, 8)
+    return false 
+
+  elsif 
+    board[index] == " "||board[index] == ""|| board[index] == nil && index.between?(0, 8)
+    return true 
+
+  else 
+    return false
+
+ end
+end
