de1d6998267f81cdcb5d7f3d12ef723f3d8241c3
Karim Alomari <karim.alomari@gmail.com>
2020-07-13 16:19:32 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,20 +6,25 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+
 def input_to_index(user_input)
   user_input.to_i-1
 end
 
+
 def move (board, position, char="X")
   board[position] = char
 end
+
 
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
       taken = false
   elsif board[index] == "X" || board[index] == "O"
       taken = true
+  end
 end
+
 
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
